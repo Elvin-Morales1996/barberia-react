06a9867ce6b3189fe9cfbae55d0a77d70fc2eaab
Elvin-Morales1996 <elvinmorales.hernandez96@gmail.com>
2023-09-13 18:08:35 -0600

    e

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePages/>}></Route>
       <Route path="/about" element={<AboutPage/>}></Route>
-      <Route path="/crea" element={<CreatePages/>}></Route>
+  
       
 
     </Routes>
