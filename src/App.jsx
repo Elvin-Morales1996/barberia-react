d1ddb86c4483ba73afd3fb5d0313410e2fa6ec2a
Elvin-Morales1996 <elvinmorales.hernandez96@gmail.com>
2023-09-13 18:23:11 -0600

    app

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePages from "./pages/HomePages"
 import AboutPage from "./pages/AboutPage"
-import { Clients } from "./pages/Clients"
-
-
-
+import PatientsPage from "./pages/patientsPages"
+import CreatePages from "./pages/CreatePages"
 
 
 
@@ -20,13 +18,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePages/>}></Route>
       <Route path="/about" element={<AboutPage/>}></Route>
-<<<<<<< HEAD
-      <Route path="/clients" element={<Clients/>}></Route>
-      
-=======
-  
->>>>>>> 06a9867ce6b3189fe9cfbae55d0a77d70fc2eaab
-      
+
 
     </Routes>
     </BrowserRouter>
